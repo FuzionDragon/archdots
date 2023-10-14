0d2df03c137127d988d36c6b8fcbc782a68ed805
David L <120467347+FuzionDragon@users.noreply.github.com>
2023-10-14 17:45:14 -0100

    first backup 'data'

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+dotfiles="$HOME/dotfiles"
+
+ln -sr auto-cpufreq ~/.config/auto-cpufreq
+ln -sr kitty ~/.config/kitty
+ln -sr nvim ~/.config/nvim
+ln -sr rofi ~/.config/rofi
+ln -sr swaylock ~/.config/swaylock
+ln -sr mako ~/.config/mako
+ln -sr waybar ~/.config/waybar
+ln -sr Wallpapers ~/Wallpapers
+ln -s oh-my-zsh ~/.oh-my-zsh
+ln -s zshrc ~/.zshrc
+
