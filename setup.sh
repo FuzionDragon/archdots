88b1c6ba04e0dea3771bb19b4c79fbf36dd1683e
David L <120467347+FuzionDragon@users.noreply.github.com>
2023-10-16 12:58:51 -0100

    New backup 2023-10-16 12:58:51

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 dotfiles="$HOME/dotfiles"
 
+# Paru Install
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# Package Install
+paru -S rust lld gparted grim git gimp alsa-utils amd-ucode auto-cpufreq autojump-rs base base obsidian npm networkmanager ntfs-3g pfetch-rs polkit-gnome preload rofi timeshift vlc waybar-hyprland xdg-desktop-portal-hyprland xorg-xhost zsh reflector syncthing ttf-firacode-nerd ttf-font-awesome ttf-meslo-nerd ufw openbsd-netcat obs-studio neovim man-db ncspot mako linux-lts linux-firmware linux libreoffice-fresh grub dolphin dmidecode cmake efibootmgr htop hyprland gimp krita firefox iptables-nft brightnessctl bridge-utils armcord kitty spdlog sof-firmware wireplumber steam qt6-wayland swayidle swaylock-effects swww 
+
+# Symlink Configs
 sudo ln -sr auto-cpufreq ~/.config/auto-cpufreq
 ln -sr kitty ~/.config/kitty
 ln -sr nvim ~/.config/nvim
@@ -14,5 +24,10 @@ ln -s oh-my-zsh ~/.oh-my-zsh
 ln -s zshrc ~/.zshrc
 ln -sr ncspot ~/.config/ncspot
 ln -sr hypr ~/.config/hypr
+rm -r ~/.mozilla/firefox && ln -s firefox ~/.mozilla/firefox
 
+# Reflector Mirrors
+reflector --verbose --country 'United Kingdom' -l 10 rate --save /etc/pacman.d/mirrorlist
+sudo systemctl enable reflector.timer
+sudo systemctl start reflector.timer
 
