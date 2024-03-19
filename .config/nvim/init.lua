ad539d0218224026c8af61b3b6e7de5adf5dccfa
David L <120467347+FuzionDragon@users.noreply.github.com>
2024-03-19 15:47:00 +0000

    New backup 2024-03-19 15:47:00

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,8 +67,8 @@ require('lazy').setup({
     respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
     easing_function = nil,       -- Default easing function
-    pre_hook = nil,              -- Function to run before the scrolling animation starts
-    post_hook = nil,             -- Function to run after the scrolling animation ends
+    pre_hook = true,              -- Function to run before the scrolling animation starts
+    post_hook = true,             -- Function to run after the scrolling animation ends
     performance_mode = false,    -- Disable "Performance Mode" on all buffers.
   },
 
@@ -166,6 +166,11 @@ require('lazy').setup({
     -- See `:help ibl`
     main = 'ibl',
     opts = {},
+  },
+
+  {
+     'christoomey/vim-tmux-navigator',
+    lazy = false,
   },
 
   -- Fancy Startpage
